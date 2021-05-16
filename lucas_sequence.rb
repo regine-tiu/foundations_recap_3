631747e03714f01976968338503b9a4a69a8ded0
regine-tiu <regine.d.tiu@gmail.com>
2021-05-16 19:28:38 +0800

    solved

--- lucas_sequence.rb
+++ lucas_sequence.rb
@@ -1,12 +1,16 @@
-def lucas_sequence(num)
-  arr = []
-  return arr if num == 0
-  arr << 2 if num == 1
-  
-  (0...num.length).each |i|
-    arr << (lucas_sequence[i-1] + lucas_sequence[i-2])
-  end
-  arr
+def lucas_sequence(length)
+  return [] if length == 0
+  return [2] if length == 1
+  return [2, 1] if length == 2
+
+  seq = lucas_sequence(length -1)
+  next_el = seq[-1] + seq[-2]
+  seq << next_el
 end
 
-p lucas_sequence(0)
+p lucas_sequence(0)   # => []
+p lucas_sequence(1)   # => [2]    
+p lucas_sequence(2)   # => [2, 1]
+p lucas_sequence(3)   # => [2, 1, 3]
+p lucas_sequence(6)   # => [2, 1, 3, 4, 7, 11]
+p lucas_sequence(8) 
